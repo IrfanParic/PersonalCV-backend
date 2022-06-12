e0af98afe829f12879a926a55659d80e2c44025f
IrfanParic <irfan.paric@stu.ssst.edu.ba>
2022-06-12 15:21:00 +0200

    Added: Setup of backend

---
+++ src/main/java/com/example/CVbackend/CvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CVbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CVbackend/CvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CVbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
